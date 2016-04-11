2392689e1ce00fd07cc5d29014a4be19ee91eee0
ndennler <nsdennler@wpi.edu>
2016-04-11 16:20:40 -0400

    added the builder stock panel

---
+++ sarpedon/sarpedon/src/boundary/builder/BuilderStockPanel.java
@@ -0,0 +1,16 @@
+package boundary.builder;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class BuilderStockPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public BuilderStockPanel() {
+		setBackground(Color.WHITE);
+
+	}
+
+}

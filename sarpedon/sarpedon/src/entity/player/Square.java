208f143340788d7f673e280b3b6d353c7ae45f1c
dcicc <dcicc@130.215.125.54>
2016-04-14 12:33:43 -0400

    added flip and rotate

--- sarpedon/sarpedon/src/entity/player/Square.java
+++ sarpedon/sarpedon/src/entity/player/Square.java
@@ -16,6 +16,46 @@ public class Square {
 	public int getY(){
 		return this.yFromAnchor;
 	}
+	void rotateClockwise()
+	{
+		int currentY = yFromAnchor;
+		if(xFromAnchor > 0)
+		{
+			yFromAnchor = xFromAnchor;
+		}
+		else 
+			yFromAnchor = -1*xFromAnchor;
+		if(yFromAnchor >0)
+		{
+			xFromAnchor = -1* currentY;
+		}
+		else
+			xFromAnchor =  currentY;
+	}
+	
+	void rotateCounter()
+	{
+		int currentY = yFromAnchor;
+		if(xFromAnchor > 0)
+		{
+			yFromAnchor = -1*xFromAnchor;
+		}
+		else 
+			yFromAnchor =  xFromAnchor;
+		if(yFromAnchor >0)
+		{
+			xFromAnchor =  currentY;
+		}
+		else
+			xFromAnchor = -1* currentY;
+	}
 
-
+	void flipHorizontal()
+	{
+		xFromAnchor = -1*xFromAnchor;
+	}
+	void flipVertical()
+	{
+		yFromAnchor = -1*yFromAnchor;
+	}
 }

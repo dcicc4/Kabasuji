ef5bebd235f921720ea7444f5b0693262c260ddf
Wyatt <ehhenke@wpi.edu>
2016-04-18 16:39:00 -0400

    Added ReturnToMenuController

---
+++ sarpedon/sarpedon/src/control/player/ReturnToMenuController.java
@@ -0,0 +1,16 @@
+package control.player;
+
+import bounday.player.KabasujiMenuGui;
+
+public class ReturnToMenuController {
+	KabasujiMenuGui parentPanel;
+	
+	ReturnToMenuController(KabasujiMenuGui parentPanel) {
+		this.parentPanel = parentPanel;
+	}
+	
+	void quit() {
+		
+	}
+	
+}

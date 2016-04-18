179a3cd7afb4b8fd86d773393be70f48498e7577
Wyatt <ehhenke@wpi.edu>
2016-04-18 16:45:43 -0400

    Added ResetController

---
+++ sarpedon/sarpedon/src/control/player/ResetController.java
@@ -0,0 +1,16 @@
+package control.player;
+
+import bounday.player.PlayerBoardPanel;
+
+public class ResetController {
+	PlayerBoardPanel parentPanel;
+	
+	ResetController(PlayerBoardPanel parentPanel) {
+		this.parentPanel = parentPanel;
+	}
+	
+	void reset() {
+		
+	}
+	
+}

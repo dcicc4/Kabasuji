7917e028133e4fc1600d57460264271aab4931df
ttshiz <ttshiz@wpi.edu>
2016-04-20 14:21:04 -0400

    Commented Tile Class, started TileTest

--- sarpedon/sarpedon/src/entity/player/Tile.java
+++ sarpedon/sarpedon/src/entity/player/Tile.java
@@ -2,41 +2,50 @@ package entity.player;
 
 import java.io.Serializable;
 import java.util.UUID;
-
+/**
+ *  A basic component of a board.
+ *  Contains the row and column where the tile is located as well as a unique identifier
+ *  for the Piece covering the Tile or null if the Tile is not covered by a Piece.
+ * @Tesia Shizume (ttshiz@wpi.edu)
+ */
 public class Tile implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	// a tile with it's lower right corner denoted by the tuple (row, col)
-	Integer row; 
-	Integer col;
-	UUID coveredBy; //stores the unique ID of the piece covering it
 	
+	private static final long serialVersionUID = 1L;
+	/** the row in where the lower right corner of the tile is */
+	Integer row; 
+	/** the column in where the lower right corner of the tile is */
+	Integer col;
+	/** the unique ID of the piece covering it */
+	UUID coveredBy; 
+	
+	/** Constructor with UUID provided */
 	public Tile(Integer rw, Integer cl, UUID cvrd){
 		row = rw;
 		col = cl;
 		coveredBy = cvrd;
 	}
-	
+	/** Constructor without UUID */
 	public Tile(Integer rw, Integer cl){
 		row = rw;
 		col = cl;
 		coveredBy = null;
 	}
 	
+	/** Gets the row location of the Tile */
 	Integer getRow(){
 		return this.row;
 	}
-	
+	/** Gets the column location of the Tile */
 	Integer getColumn(){
 		return this.col;
 	}
 	
+	/** Gets the UUID of the Piece that covers the Tile */
 	UUID getCoveredBy(){
 		return this.coveredBy;
 	}
 	
+	/** Sets the UUID of the Piece that covers the Tile */
 	void setCoveredBy(UUID i){
 		if (i.compareTo(null) == 0){
 			this.coveredBy = i;
@@ -46,7 +55,7 @@ public class Tile implements Serializable {
 		}
 	}
 	
-	// checks if a location is over the tile
+	/** Checks if a location is over the tile, returns true if it is */
 	boolean overTile(Integer rw, Integer cl){
 		if ((rw <= this.row) && (rw > (this.row - 1)) && (cl <= this.col) && (cl > (this.col -1))){
 			return true;
---
+++ sarpedon/sarpedon/test/entity/player/TileTest.java
@@ -0,0 +1,36 @@
+package entity.player;
+
+import junit.framework.TestCase;
+
+public class TileTest extends TestCase {
+
+	public void testTileIntegerIntegerUUID() {
+		fail("Not yet implemented");
+	}
+
+	public void testTileIntegerInteger() {
+		Tile testTile = new Tile(0,0);
+		assertTrue(testTile.col == 0);
+	}
+
+	public void testGetRow() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetColumn() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCoveredBy() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetCoveredBy() {
+		fail("Not yet implemented");
+	}
+
+	public void testOverTile() {
+		fail("Not yet implemented");
+	}
+
+}

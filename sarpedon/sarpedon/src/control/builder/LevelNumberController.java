82d52ca8d3629ba80ca4be77d6738a800a7a2773
dcicc <dcicc@130.215.125.54>
2016-04-20 14:34:27 -0400

    Working loading and merged

--- sarpedon/sarpedon/src/control/builder/LevelNumberController.java
+++ sarpedon/sarpedon/src/control/builder/LevelNumberController.java
@@ -6,6 +6,11 @@ import entity.player.*;
 
 import javax.swing.*;
 
+import bounday.player.LightningLevelGui;
+import bounday.player.PlayerBullpenPanel;
+import bounday.player.PuzzleLevelGui;
+import bounday.player.ReleaseLevelGui;
+
 public class LevelNumberController implements ActionListener{
 
 
@@ -44,8 +49,7 @@ JFrame LG;
 	
 	public JFrame getFrame(){
 return LG;
-	public void changeLevelNumber(Integer number){
-		level.setNumber(number);
+	}
 	public void changeLevelNumber(Integer number){
 		level.setNumber(number);
 	}

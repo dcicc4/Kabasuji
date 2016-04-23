ea8e0d6d7aa535f7283a42ca4da92f78c1db7e80
ttshiz <ttshiz@wpi.edu>
2016-04-23 19:58:54 -0400

    Changed string representation of color to enumeration in ReleaseTile; added it's test case

--- sarpedon/sarpedon/src/entity/player/ReleaseTile.java
+++ sarpedon/sarpedon/src/entity/player/ReleaseTile.java
@@ -4,20 +4,44 @@ import java.util.UUID;
 
 public class ReleaseTile extends Tile {
 	Integer rnumber; // the number associated with the instance of the Release Tile
-	String rcolor; // the color of the instance of the ReleaseTile
+	public enum Color {
+		BLUE, GREEN, RED
+	}
+	Color rcolor; // the color of the instance of the ReleaseTile
+	
 
+	/** Gets the number assigned to the Tile */
 	Integer getNumber(){
 		return this.rnumber;
 	}
-	
-	String getColor(){
+	/** Gets the colour assigned to the Tile */
+	Color getColor(){
 		return this.rcolor;
 	}
-	
-	public ReleaseTile(Integer rw, Integer cl, UUID cvrd, Integer rnum, String rcolr) {
+	/** Constructor with UUID provided */
+	public ReleaseTile(Integer rw, Integer cl, UUID cvrd, Integer rnum, Color rcolr) {
 		super(rw, cl, cvrd);
 		rnumber = rnum;
 		rcolor = rcolr;
 	}
+	/** Constructor without UUID */
+	public ReleaseTile(Integer rw, Integer cl, Integer rnum, Color rcolr) {
+		super(rw, cl);
+		rnumber = rnum;
+		rcolor = rcolr;
+	}
 
+	/** Provides a string representation of the Color */
+	public String getColorString(){
+		switch(this.rcolor){
+		case BLUE:
+			return "BLUE";
+		case GREEN:
+			return "GREEN";
+		case RED:
+			return "RED";
+		default:
+			throw new RuntimeException("entity.player::ReleaseTile: Unexpected Color");
+		}
+	}
 }
---
+++ sarpedon/sarpedon/test/entity/player/ReleaseTileTest.java
@@ -0,0 +1,58 @@
+package entity.player;
+
+import java.util.UUID;
+
+import junit.framework.TestCase;
+
+public class ReleaseTileTest extends TestCase {
+	/** A arbitrary tile without an UUID */
+	ReleaseTile testTileOrigin;
+	
+	/** A arbitrary tile with a UUID */
+	UUID testUUIDa;
+	ReleaseTile testTile11n9UUID;
+	
+	/** creates Tiles for testing */
+	protected void setUp() throws Exception {
+		testUUIDa = UUID.randomUUID();
+		testTileOrigin = new ReleaseTile(0,0,1, ReleaseTile.Color.BLUE);
+    	testTile11n9UUID = new ReleaseTile(11,9,testUUIDa, 3, ReleaseTile.Color.RED);
+    }
+	
+	public void testGetNumber() {
+		assertTrue(testTileOrigin.rnumber == testTileOrigin.getNumber());
+		assertTrue(testTile11n9UUID.rnumber == testTile11n9UUID.getNumber());
+	}
+
+	public void testGetColor() {
+		assertTrue(testTileOrigin.rcolor == testTileOrigin.getColor());
+		assertTrue(testTile11n9UUID.rcolor == testTile11n9UUID.getColor());
+	}
+
+	public void testReleaseTile() {
+		UUID testUUID;
+		for (int i = 0; i <= 11; i++){
+			for (int j = 0; j <= 11; j++){
+				for (int k = 1; k <=6; k++){
+					for(ReleaseTile.Color c: ReleaseTile.Color.values()){
+						testUUID = UUID.randomUUID();
+						ReleaseTile testTile = new ReleaseTile(i,j, testUUID, k, c);
+						assertTrue(testTile.col == j);
+						assertTrue(testTile.row == i);
+						assertTrue(testTile.coveredBy == testUUID);
+						assertTrue(testTile.rnumber == k);
+						assertTrue(testTile.rcolor == c);
+					}
+				}
+			}
+		}
+	}
+	
+	public void testGetColorString(){
+		assertTrue("BLUE".compareTo(testTileOrigin.getColorString())==0);
+		assertTrue("RED".compareTo(testTile11n9UUID.getColorString())==0);
+		ReleaseTile testTile = new ReleaseTile(6,6,3,ReleaseTile.Color.GREEN);
+		assertTrue("GREEN".compareTo(testTile.getColorString())==0);
+	}
+
+}

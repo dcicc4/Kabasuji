349f58420ac5952b24ab2736c91f62073ba4c261
Brandon Kelly <Brandon Kelly@130.215.169.191>
2016-04-27 13:58:59 -0400

    Added some boundary tests

--- sarpedon/sarpedon/test/boundary/player/PlayerBoundariesTest.java
+++ sarpedon/sarpedon/test/boundary/player/PlayerBoundariesTest.java
@@ -1,7 +1,12 @@
 package boundary.player;
 
+import Main.PlayerApplication;
 import junit.framework.TestCase;
 
 public class PlayerBoundariesTest extends TestCase {
-
+	public void testPlayer(){
+		PlayerApplication game = new PlayerApplication();
+		game.main(null);
+		assertEquals(null,null);
+	}
 }

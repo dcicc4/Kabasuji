32fe933f4689f99a6227255c88afa87bfe96c42c
Brandon Kelly <Brandon Kelly@130.215.236.165>
2016-04-27 15:14:34 -0400

    Got coverage to about 85% on both boundary tests, still working on it.

--- sarpedon/sarpedon/test/boundary/builder/BuilderBoundaryTests.java
+++ sarpedon/sarpedon/test/boundary/builder/BuilderBoundaryTests.java
@@ -9,15 +9,33 @@ import junit.framework.TestCase;
 //import javax.swing.SwingConstants;
 //import javax.swing.border.EmptyBorder;
 import Main.BuilderApplication;
-//import boundary.builder.*;
+import boundary.builder.*;
 //import javax.swing.JFrame;
 //import javax.swing.JLabel;
+import entity.builder.BuildableLightning;
+import entity.builder.BuildablePuzzle;
+import entity.builder.BuildableRelease;
+
 
 public class BuilderBoundaryTests extends TestCase {
+
+
 	
 	public void testBuilderBoundry(){
 		BuilderApplication game = new BuilderApplication();
 		game.main(null);
+		
+		BuildableLightning model = new BuildableLightning();
+		LightningBuilderGui lbg = new LightningBuilderGui(model);
+		lbg.setVisible(true);
+		
+		BuildablePuzzle model2 = new BuildablePuzzle();
+		PuzzleBuilderGui pbg = new PuzzleBuilderGui(model2);
+		pbg.setVisible(true);
+		
+		BuildableRelease model3 = new BuildableRelease();
+		ReleaseBuilderGui rbg = new ReleaseBuilderGui(model3);
+		rbg.setVisible(true);
 		
 		assertEquals(null,null);
 	}

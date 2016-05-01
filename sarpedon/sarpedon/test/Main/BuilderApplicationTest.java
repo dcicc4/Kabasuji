3f3a109bef816ed902a1c210fabcd960e6a4e38b
ttshiz <ttshiz@wpi.edu>
2016-05-01 19:37:52 -0400

    added testing for BuilderApplication: initializeControllers

---
+++ sarpedon/sarpedon/test/Main/BuilderApplicationTest.java
@@ -0,0 +1,22 @@
+package Main;
+
+import control.builder.SwitchWindowController;
+import junit.framework.TestCase;
+
+public class BuilderApplicationTest extends TestCase {
+	BuilderApplication bldApp;
+	protected void setUp() throws Exception {
+		super.setUp();
+		bldApp = new BuilderApplication();
+	}
+
+	public void testInitializeControllers() {
+		bldApp.initializeControllers();
+		assertTrue(bldApp.levelBuilder.getLButton().getActionListeners()[0] instanceof SwitchWindowController);
+		assertTrue(bldApp.levelBuilder.getPButton().getActionListeners()[0] instanceof SwitchWindowController);
+		assertTrue(bldApp.levelBuilder.getRButton().getActionListeners()[0] instanceof SwitchWindowController);
+		assertTrue(bldApp.levelBuilder.getEButton().getActionListeners()[0] instanceof SwitchWindowController);
+	}
+
+
+}

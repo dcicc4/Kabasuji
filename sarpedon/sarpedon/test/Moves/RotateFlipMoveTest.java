56b75fe11b7524e9b81271109b01213e38ae101f
ttshiz <ttshiz@wpi.edu>
2016-05-02 17:40:04 -0400

    added testing for HintMove and RotateFlip

---
+++ sarpedon/sarpedon/test/Moves/RotateFlipMoveTest.java
@@ -0,0 +1,21 @@
+package Moves;
+
+import entity.player.Bullpen;
+import junit.framework.TestCase;
+
+public class RotateFlipMoveTest extends TestCase {
+	Bullpen testBull;
+	protected void setUp() throws Exception {
+		super.setUp();
+		testBull = new Bullpen();
+	}
+
+	public void testRotateFlipMove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testUndo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}

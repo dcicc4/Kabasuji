5b5242b6385ea1eb0ca10d0b8ec584eb6d13a07b
Brandon Kelly <Brandon Kelly@130.215.236.165>
2016-05-03 04:50:26 -0400

    Documentation for Boundary Tests

--- sarpedon/sarpedon/test/boundary/player/PlayerBoundariesTest.java
+++ sarpedon/sarpedon/test/boundary/player/PlayerBoundariesTest.java
@@ -15,26 +15,27 @@ import junit.framework.TestCase;
 
 public class PlayerBoundariesTest extends TestCase {
 	public void testPlayer(){
-		PlayerApplication game = new PlayerApplication();
-		game.main(null);
+		PlayerApplication game = new PlayerApplication(); //constructs the player application
+		game.main(null); //runs the player application
 		
-		SarpedonKabasuji g = new SarpedonKabasuji(); 
-		Bullpen bp = new Bullpen();
-		Board b = new Board();
-		LightningBoard lb = new LightningBoard();
+		SarpedonKabasuji g = new SarpedonKabasuji(); //constructs a new game
+		Bullpen bp = new Bullpen(); //constructs an empty bullpen
+		Board b = new Board(); //constructs an empty board
+		LightningBoard lb = new LightningBoard(); //constructs an empty lightning board
 		
-		PuzzleLevel pl = new PuzzleLevel(b, bp, null);
-		PuzzleLevelGui plg = new PuzzleLevelGui(pl, g);
-		plg.setVisible(true);
+		PuzzleLevel pl = new PuzzleLevel(b, bp, null); //constructs a new puzzle level
+		PuzzleLevelGui plg = new PuzzleLevelGui(pl, g); //constructs a new puzzle level GUI
+		plg.setVisible(true); //ensures GUI is run and displays
 		
-		ReleaseLevel rl = new ReleaseLevel();
-		ReleaseLevelGui rlg = new ReleaseLevelGui(rl, g);
-		rlg.setVisible(true);
+		ReleaseLevel rl = new ReleaseLevel(); //constructs a new release level
+		ReleaseLevelGui rlg = new ReleaseLevelGui(rl, g); //constructs a new release level GUI
+		rlg.setVisible(true); //ensures GUI is run and displays
 		
-		LightningLevel ll = new LightningLevel(lb, bp, null, false, null, 7, 5);
-		LightningLevelGui llg = new LightningLevelGui(ll, g);
-		llg.setVisible(true);
+		LightningLevel ll = new LightningLevel(lb, bp, null, false, null, 7, 5); //constructs a new lightning level
+		LightningLevelGui llg = new LightningLevelGui(ll, g); //constructs a new lightning level GUI
+		llg.setVisible(true); //ensures GUI is run and displays
 		
+		//test to confirm objects are being created and working correctly
 		assertEquals(bp.piecesLeft(),0);
 	}
 }
